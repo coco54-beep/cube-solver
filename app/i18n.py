c1e0732922fae1ff9f5ccadcdc5c119e5f91cc00
coco54-beep <coco54-beep@users.noreply.github.com>
2026-09-11 08:30:32 +0800

    chore: 版本号 1.2.1 -> 1.2.2

--- app/i18n.py
+++ app/i18n.py
@@ -8,7 +8,7 @@
 用法：
     from app.i18n import tr
     label.text = tr("home.help")
-    label.text = tr("home.version", version="1.2.1")
+    label.text = tr("home.version", version="1.2.2")
 """
 
 import os
